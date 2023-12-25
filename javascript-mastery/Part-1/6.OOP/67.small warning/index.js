f7b1e2ccaf7fa3c0c3e20aa5884b57812a8c262a
Tutul Kabir <tutulkabir@gmail.com>
2023-12-25 11:18:03 +0530

    arrow and this

--- javascript-mastery/Part-1/6.OOP/67.small warning/index.js
+++ javascript-mastery/Part-1/6.OOP/67.small warning/index.js
@@ -1,19 +1,16 @@
 // small warning
 
-
 const user1 = {
-    firstName: "Tutul",
-    age:20,
-    about: function(){
-        console.log(this.firstName, this.age);
-    }
-}
-
+  firstName: "Tutul",
+  age: 20,
+  about: function () {
+    console.log(this.firstName, this.age);
+  },
+};
 
 // don't do this mistae
 // const myFunc = user1.about;
 // myFunc() //undefined undefined
-
 
 //do this
 const myFunc = user1.about.bind(user1);
--- javascript-mastery/Part-1/6.OOP/68.Arrow function ans this/index.js
+++ javascript-mastery/Part-1/6.OOP/68.Arrow function ans this/index.js
@@ -2,11 +2,14 @@
 // arrow function ka khudka this nehi hota.. jo leta hai wo sorrounding se lete hai
 // arrow function ka this window hoga
 const user1 = {
-    firstName : "Tutul",
-    age:29,
-    about: () => {
-        console.log(this.firstName, this.age);
-    }
-}
+  firstName: "Tutul",
+  age: 29,
+  about: () => {
+    console.log(this); // window object
+    console.log(this.firstName, this.age);
+  },
+};
 
-user1.about(); //
+user1.about(); // undefined undefined
+
+//call, apply, bind method use korle o arrow function er this change korte parbo na // arrow function er this change kora jai na
